efd81f73d9cb7844089c0eadb103c3aac23091c1
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2021-11-04 23:36:50 -0300

    Initial commit

---
+++ src/main/java/me/duncanruns/excpi/mixin/DebugHudMixin.java
@@ -0,0 +1,28 @@
+package me.duncanruns.excpi.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.DebugHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DebugHud.class)
+public abstract class DebugHudMixin extends DrawableHelper {
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void leftTextMixin(MatrixStack matrices, CallbackInfo info) {
+        if (client.options.debugProfilerEnabled) {
+            client.getProfiler().push("debug");
+            client.getProfiler().pop();
+            info.cancel();
+        }
+    }
+}
